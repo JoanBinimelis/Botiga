589f2929717975284815ca07adba8b02a20895b3
JoanBinimelis <46893257+JoanBinimelis@users.noreply.github.com>
2020-11-17 13:47:06 +0100

    euros

--- public/index.php
+++ public/index.php
@@ -31,7 +31,7 @@ $resultat = $conexio -> query($consulta);
     <div class="card" style="width: 18rem;">
       <img src="/img/<?php echo $id ?>.jpg" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title"><?php echo $nom;?></h5>
+        <h5 class="card-title"><?php echo $nom;?>€</h5>
         <p><?php echo $preu;?></p>
         <a href="detalls.php?id=<?php echo $row['id'];?>" class="btn btn-primary">Detalls</a>
       </div>
