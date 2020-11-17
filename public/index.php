c74757256eced4a670e0d1f7772b9d1deccaf65b
JoanBinimelis <46893257+JoanBinimelis@users.noreply.github.com>
2020-11-17 13:32:08 +0100

    cards

--- public/index.php
+++ public/index.php
@@ -16,7 +16,34 @@ $resultat = $conexio -> query($consulta);
       $nom = $row['Nom'];
       $id = $id['id'];
       ?>
-      <div>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+  <body>
+
+    <div class="card" style="width: 18rem;">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title"><?php echo $nom;?></h5>
+        <a href="detalls.php?id=<?php echo $row['id'];?>" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+
+
+  </body>
+</html>
+
+
+      -------------------------------
+
+      -------------------------
+      <!--<div>
         <br>
         <table>
           <tr> <?php echo $nom;?> </tr><br>
@@ -25,7 +52,7 @@ $resultat = $conexio -> query($consulta);
             <input type="button" value="Detalles del producto"/>
           </a>
           <br>
-      </div>
+      </div>-->
 
       <?php
     }
