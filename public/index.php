5583dd3f733a66eeda35da10d15f6f8f671c31eb
JoanBinimelis <46893257+JoanBinimelis@users.noreply.github.com>
2020-11-17 13:41:11 +0100

    canvis

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@ $resultat = $conexio -> query($consulta);
 
     while ($row = $resultat -> fetch_array()) {
       $nom = $row['Nom'];
-      $id = $id['id'];
+      $id = $row['id'];
       ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -28,10 +28,10 @@ $resultat = $conexio -> query($consulta);
   <body>
 
     <div class="card" style="width: 18rem;">
-      <img src="img/<?php echo $id ?>.jpg" class="card-img-top" alt="...">
+      <img src="/img/<?php echo $id ?>.jpg" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title"><?php echo $nom;?></h5>
-        <a href="detalls.php?id=<?php echo $row['id'];?>" class="btn btn-primary">Go somewhere</a>
+        <a href="detalls.php?id=<?php echo $row['id'];?>" class="btn btn-primary">Detalls</a>
       </div>
     </div>
 
