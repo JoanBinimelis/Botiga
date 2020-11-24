5e4726fb946cbbe9ad315bdd1bc9d063ed5c621a
JoanBinimelis <46893257+JoanBinimelis@users.noreply.github.com>
2020-11-24 11:22:30 +0100

    col

--- public/index.php
+++ public/index.php
@@ -23,7 +23,7 @@ $resultat = $conexio -> query($consulta);
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
   <body>
-    <div class="card">
+    <div class="card-columns">
       <?php
       while ($row = $resultat -> fetch_array()) {
         $nom = $row['Nom'];
