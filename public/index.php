36f4b7b81b765f8db68107f3791a4ef5bcb4a66d
JoanBinimelis <46893257+JoanBinimelis@users.noreply.github.com>
2020-11-24 13:49:46 +0100

    as

--- public/index.php
+++ public/index.php
@@ -21,6 +21,7 @@ $resultat = $conexio->query($consulta);
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   </head>
   <body>
+    <div class="container">
 
 <?php
 if ($resultat) {
@@ -29,7 +30,8 @@ if ($resultat) {
     $id = $row['id'];
     $preu = $row['Preu'];
   ?>
-        <div class="container">
+
+
             <div class="card">
               <img src="/img/<?php echo $id;?>.jpg" class="card-img-top" alt="...">
               <div class="card-body">
@@ -38,7 +40,9 @@ if ($resultat) {
                 <a href="detalls.php?id=<?php echo $id;?>" class="btn btn-primary">Detalls</a>
               </div>
             </div>
+
           </div>
+
    </body>
   </html>
 <?php
