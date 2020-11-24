0287dcbe77762a4170516fc907ccdaa042ef5ba2
JoanBinimelis <46893257+JoanBinimelis@users.noreply.github.com>
2020-11-24 11:45:47 +0100

    a robar carteras

--- public/index.php
+++ public/index.php
@@ -23,13 +23,15 @@ $resultat = $conexio -> query($consulta);
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
   <body>
-    <div class="container">
+
       <?php
+
       while ($row = $resultat -> fetch_array()) {
         $nom = $row['Nom'];
         $id = $row['id'];
         $preu = $row['Preu'];
         ?>
+        <div class="container">
       <div class="card" style="width: 18rem;">
         <img src="/img/<?php echo $id ?>.jpg" class="card-img-top" alt="...">
         <div class="card-body">
@@ -38,9 +40,10 @@ $resultat = $conexio -> query($consulta);
           <a href="detalls.php?id=<?php echo $row['id'];?>" class="btn btn-primary">Detalls</a>
         </div>
       </div>
+      </div>
       <?php
        ?>
-    </div>
+
   </body>
 </html>
 <?php
